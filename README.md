62bd8c7b8d905cb9d69cb0533d8985591f311ba1
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-07-17 20:36:30 -0500

    Update package information

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # NSO-Applet-API
 
-[See the documentation](/lp1.nso.nintendo.net.md)
+An API library and documentation for the NSO Applet on the Nintendo Switch.
+
+This library implements various endpoints from the NSO Webapplet, with the intention of completely covering all endpoints and documenting them.
+
+[See the documentation here](/lp1.nso.nintendo.net.md)
 
 You can download this library from [PyPI](https://pypi.org/project/nso-applet-api/) by running the following in your Python environment:
 ```sh
--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 # Made by Deltaion Lee (MCMi460) on Github
 import setuptools
 
+from pathlib import Path
+directory = Path(__file__).parent
+long_description = (directory / 'README.md').read_text()
+
 setuptools.setup(
 	name = 'nso-applet-api',
 	version = '0.0.1',
-	description = 'Nintendo NSO applet API',
-	long_description = 'This library implements in Python various API endpoints from the NSO Webapplet',
+	description = 'Nintendo NSO Applet API',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
 	author = 'Deltaion Lee',
 	author_email = '32529306+MCMi460@users.noreply.github.com',
 	url = 'https://github.com/MCMi460/nso-applet-api',
